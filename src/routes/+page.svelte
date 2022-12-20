4a03e08bd00bb3d59bf589d92c1c0a2fb504e30b
Kyle Nickel <1135915+nickelkr@users.noreply.github.com>
2022-12-19 22:12:15 -0500

    Naive note

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import { browser }  from "$app/environment";
+
+  let scratch = "";
+  let timer;
+
+  if (browser) {
+    scratch = localStorage.getItem('thunks') || "";
+  }
+
+  const debounce = () => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      localStorage.setItem("thunks", scratch);
+      console.log(scratch);
+    }, 750);
+
+  }
+</script>
+
+<div contenteditable="true" bind:innerHTML={scratch} on:keyup={() => debounce() }></div>
